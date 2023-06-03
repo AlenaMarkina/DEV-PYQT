ccdfe1166587e1475c39bfe6b262a95859af3984
Alena Markina <markina.a7@yandex.ru>
2023-06-03 14:43:38 +0300

    Home work 2 finished

--- home_work_2/d_eventfilter_settings.py
+++ home_work_2/d_eventfilter_settings.py
@@ -13,7 +13,7 @@
 + 3. Для QLCDNumber сделать отображение в различных системах счисления (oct, hex, bin, dec),
    изменять формат отображаемого значения в зависимости от выбранного в comboBox параметра.
 
-4. Сохранять значение выбранного в comboBox режима отображения
++ 4. Сохранять значение выбранного в comboBox режима отображения
    и значение LCDNumber в QSettings, при перезапуске программы выводить
    в него соответствующие значения
 """
@@ -30,69 +30,93 @@ class Window(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.settings = QtCore.QSettings("Home work 2, d_eventfilter_settings.py")
+        self.settings = QtCore.QSettings("HomeWork2")
 
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
         self.initUi()
+        self.loadData()
         self.initSignal()
 
-    def initUi(self):
+    def initUi(self) -> None:
         """
+        Доинициализация окна
 
-        :return:
+        :return: None
         """
         self.setMinimumSize(600, 600)
         self.ui.comboBox.addItems(['oct', 'hex', 'bin', 'dec'])
         self.ui.dial.installEventFilter(self)
 
-    def initSignal(self):
-        """---
-
-        :return:
+    def initSignal(self) -> None:
         """
+        Инициализация сигналов
 
+        :return: None
+        """
         self.ui.dial.valueChanged.connect(self.onDialValueChanged)
         self.ui.horizontalSlider.valueChanged.connect(self.onSliderValueChanged)
         self.ui.comboBox.textActivated.connect(self.onComboBoxTextActivated)
 
-    def onComboBoxTextActivated(self):
-        number_system = self.ui.comboBox.currentText()
+    # slots --------------------------------------------------------------
+    def onComboBoxTextActivated(self) -> None:
+        """
+        Обработка сигнала 'textActivated' для виджета comboBox
+        :return: None
+        """
+        mode = self.ui.comboBox.currentText()
 
-        if number_system == 'bin':
+        if mode == 'bin':
             self.ui.lcdNumber.setBinMode()
-        elif number_system == 'hex':
+        elif mode == 'hex':
             self.ui.lcdNumber.setHexMode()
-        elif number_system == 'oct':
+        elif mode == 'oct':
             self.ui.lcdNumber.setOctMode()
-        elif number_system == 'dec':
+        elif mode == 'dec':
             self.ui.lcdNumber.setDecMode()
 
-    def onSliderValueChanged(self):
+    def onSliderValueChanged(self) -> None:
+        """
+        Обработка сигнала 'valueChanged' для виджета horizontalSlider
+        :return: None
+        """
         num = self.ui.horizontalSlider.value()
 
         self.ui.lcdNumber.display(num)
         self.ui.dial.setSliderPosition(num)
 
-    def onDialValueChanged(self):
+    def onDialValueChanged(self) -> None:
+        """
+        Обработка сигнала 'valueChanged' для виджета dial
+        :return: None
+        """
         num = self.ui.dial.value()
 
         self.ui.lcdNumber.display(num)
         self.ui.horizontalSlider.setSliderPosition(num)
 
-    # def closeEvent(self, event: QCloseEvent) -> None:
-    #     self.settings.setValue("State", self.ui.comboBox)
+    # events ---------------------------------------------------------------
+    def closeEvent(self, event: QCloseEvent) -> None:
+        """
+        Событие закрытия окна
+
+        :param event: QCloseEvent
+        :return: None
+        """
+        self.settings.setValue("combox_mode", self.ui.comboBox.currentText())
+        self.settings.setValue("lcdNumber_value", self.ui.lcdNumber.intValue())
 
     def eventFilter(self, watched: QObject, event: QEvent) -> bool:
         """
+        Обработка событий нажатия кнопок клавиатуры (+ и -) для виджета dial
 
-        :param watched:
-        :param event:
-        :return:
+        :param watched: QObject
+        :param event: QEvent
+        :return: bool
         """
-
-        # TODO: почему-то для self.ui.dial данное событие срабатывает для всех виджетов..
+        # TODO: почему-то для self.ui.dial данное событие срабатывает для всех виджетов,
+        #       хотя должно только для self.ui.dial ..
         if watched == self.ui.dial and event.type() == QEvent.Type.KeyPress:
             current_step = watched.value()
 
@@ -105,6 +129,16 @@ class Window(QtWidgets.QWidget):
                 watched.setSliderPosition(current_step)
 
         return super(Window, self).eventFilter(watched, event)
+
+    # ---------------------------------------------------------------------
+    def loadData(self) -> None:
+        """
+        Загрузка данных при открытии приложения
+
+        :return: None
+        """
+        self.ui.lcdNumber.display(self.settings.value('lcdNumber_value', ''))
+        self.ui.comboBox.setCurrentText(self.settings.value('combox_mode', ''))
 
 
 if __name__ == "__main__":
