cac427a7171c54bffb72a6b30b178fac156c1201
Маркина Алена Андреевна <a.markina@PARUS.LOCAL>
2023-06-09 15:44:33 +0300

    Home work 3 in progress

--- home_work_3/a_threads.py
+++ home_work_3/a_threads.py
@@ -9,7 +9,7 @@ from PySide6 import QtCore
 
 
 class SystemInfo(QtCore.QThread):
-    systemInfoReceived = ...  # TODO Создайте экземпляр класса Signal и передайте ему в конструктор тип данных передаваемого значения (в текущем случае list)
+    systemInfoReceived = QtCore.Signal(list)  # TODO Создайте экземпляр класса Signal и передайте ему в конструктор тип данных передаваемого значения (в текущем случае list)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -17,17 +17,19 @@ class SystemInfo(QtCore.QThread):
 
     def run(self) -> None:  # TODO переопределить метод run
         if self.delay is None:  # TODO Если задержка не передана в поток перед его запуском
-            self.delay = ...  # TODO то устанавливайте значение 1
+            self.delay = 1  # TODO то устанавливайте значение 1
 
         while True:  # TODO Запустите бесконечный цикл получения информации о системе
-            cpu_value = psutil  # TODO с помощью вызова функции cpu_percent() в пакете psutil получите загрузку CPU
-            ram_value = ...  # TODO с помощью вызова функции virtual_memory().percent в пакете psutil получите загрузку RAM
-            self.systemSignal  # TODO с помощью метода .emit передайте в виде списка данные о загрузке CPU и RAM
-            time  # TODO с помощью функции .sleep() приостановите выполнение цикла на время self.delay
+            cpu_value = psutil.cpu_percent()  # TODO с помощью вызова функции cpu_percent() в пакете psutil получите загрузку CPU
+            ram_value = psutil.virtual_memory().percent  # TODO с помощью вызова функции virtual_memory().percent в пакете psutil получите загрузку RAM
+            self.systemInfoReceived.emit([cpu_value, ram_value])  # TODO с помощью метода .emit передайте в виде списка данные о загрузке CPU и RAM
+            time.sleep(self.delay)  # TODO с помощью функции .sleep() приостановите выполнение цикла на время self.delay
 
 
 class WeatherHandler(QtCore.QThread):
+    OK_STATUS = 200
     # TODO Пропишите сигналы, которые считаете нужными
+    response = QtCore.Signal(dict)
 
     def __init__(self, lat, lon, parent=None):
         super().__init__(parent)
@@ -46,14 +48,28 @@ class WeatherHandler(QtCore.QThread):
 
         self.__delay = delay
 
-    def run(self) -> None:
+    def run(self, requests=None) -> None:
         # TODO настройте метод для корректной работы
 
+        if not self.__status:
+            self.__status = 200
+
         while self.__status:
-            # TODO Примерный код ниже
-            """
-            response = requests.get(self.__api_url)
-            data = response.json()
-            ваш_сигнал.emit(data)
-            sleep(delay)
-            """
+
+            try:
+                response = requests.get(self.__api_url)
+                self.__status = response.status_code
+
+                if self.__status != self.OK_STATUS:
+                    print(f'Что-то пошло не так: status_code={self.__status}')
+                    break
+
+                data = response.json()
+                self.response.emit(data)
+                time.sleep(self.__delay)
+
+            except ConnectionError as e:
+                self.__status = None
+                print(e)
+                break
+
--- home_work_3/b_systeminfo_widget.py
+++ home_work_3/b_systeminfo_widget.py
@@ -4,10 +4,66 @@
 Создавать форму можно как в ручную, так и с помощью программы Designer
 
 Форма должна содержать:
-1. поле для ввода времени задержки
++ 1. поле для ввода времени задержки
 2. поле для вывода информации о загрузке CPU
 3. поле для вывода информации о загрузке RAM
 4. поток необходимо запускать сразу при старте приложения
 5. установку времени задержки сделать "горячей", т.е. поток должен сразу
-реагировать на изменение времени задержки
+   реагировать на изменение времени задержки
 """
+
+from PySide6 import QtWidgets, QtCore
+from PySide6.QtWidgets import QApplication, QLabel, QLineEdit, QTextEdit, \
+	QHBoxLayout, QVBoxLayout
+
+from a_threads import SystemInfo
+
+
+class MyWindow(QtWidgets.QWidget):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.initUi()
+
+	def initUi(self):
+		self.setMinimumSize(500, 300)
+
+		labelTimeDelay = QLabel()
+		labelTimeDelay.setText('Введите время задержки')
+		# labelTimeDelay.setFixedSize(188, 30)
+
+		self.lineEditTimeDelay = QLineEdit()
+		# self.lineEditTimeDelay.setFixedSize(70, 30)
+
+		self.textEdit = QTextEdit()
+		# self.textEdit.setFixedSize(200, 50)
+
+		layoutTimeDelay = QHBoxLayout()
+		layoutTimeDelay.addWidget(labelTimeDelay)
+		layoutTimeDelay.addWidget(self.lineEditTimeDelay)
+
+		layotTextEdit = QHBoxLayout()
+		layotTextEdit.addWidget(self.textEdit)
+
+		layoutMain = QVBoxLayout()
+		layoutMain.addLayout(layoutTimeDelay)
+		layoutMain.addLayout(layotTextEdit)
+
+		self.setLayout(layoutMain)
+
+
+
+	def initThread(self):
+		pass
+
+	def initSignals(self):
+		pass
+
+
+if __name__ == '__main__':
+	app = QApplication()
+
+	window = MyWindow()
+	window.show()
+
+	app.exec()
